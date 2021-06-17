71e09c2779d33dd79f620a151d87c9a7eb558020
gamerover98 <gamerover98@hotmail.it>
2021-06-17 10:11:01 +0200

    Filled and implemented the plugin configuration

--- config/src/main/java/it/gamerover/nfps/config/ConfigManager.java
+++ config/src/main/java/it/gamerover/nfps/config/ConfigManager.java
@@ -7,8 +7,15 @@ import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ConfigManager {
 
+    /**
+     * The configuration file name.
+     */
     private static final String CONFIG_FILE_NAME = "config.yml";
 
     /**
@@ -28,6 +35,174 @@ public class ConfigManager {
 
         settingsManager = YamlUtil.reloadAndGetSettings(plugin,
                 settingsManager, CONFIG_FILE_NAME, ConfigHolder.class);
+
+        double radius = getRadius();
+
+        if (radius < 0) {
+
+            String message = "The radius property cannot be a negativa value (" + radius + ")";
+
+            plugin.getLogger().warning(message);
+            setRadius(ConfigHolder.DEF_RADIUS);
+
+        }
+
+    }
+
+    //
+    // GETTER
+    //
+
+    /**
+     * @return The radius.
+     * @throws IllegalStateException If the configuration is not loaded.
+     */
+    public static double getRadius() {
+
+        checkSettings();
+
+        assert settingsManager != null;
+        return settingsManager.getProperty(ConfigHolder.RADIUS);
+
+    }
+
+    /**
+     * @return True if all normal worlds must be fixed by default
+     * @throws IllegalStateException If the configuration is not loaded.
+     */
+    public static boolean isAlwaysEnabled() {
+
+        checkSettings();
+
+        assert settingsManager != null;
+        return settingsManager.getProperty(ConfigHolder.ALWAYS_ENABLED);
+
+    }
+
+    /**
+     * @return An unmodifiable set of strings that contains the worlds' name.
+     * @throws IllegalStateException If the configuration is not loaded.
+     */
+    public static Set<String> getWorlds() {
+
+        checkSettings();
+
+        assert settingsManager != null;
+        return Collections.unmodifiableSet(settingsManager.getProperty(ConfigHolder.WORLDS));
+
+    }
+
+    /**
+     * @param worldName The world name.
+     * @return True if the world name is already contained into the config list.
+     */
+    public static boolean containsWorld(@NotNull String worldName) {
+        return getWorlds().stream().anyMatch(name -> name.equals(worldName));
+    }
+
+    //
+    // SETTER
+    //
+
+    /**
+     * @param radius A positive value of the radius.
+     * @throws IllegalArgumentException If the radius isn't a positive value.
+     * @throws IllegalStateException    If the configuration is not loaded.
+     */
+    public static void setRadius(double radius) {
+
+        checkSettings();
+
+        if (radius < 0) {
+            throw new IllegalArgumentException("The radius argument must be a positive number");
+        }
+
+        assert settingsManager != null;
+        settingsManager.setProperty(ConfigHolder.RADIUS, radius);
+
+    }
+
+    /**
+     * @param enabled A boolean value.
+     * @throws IllegalArgumentException If the radius isn't a positive value.
+     * @throws IllegalStateException    If the configuration is not loaded.
+     */
+    public static void setAlwaysEnabled(boolean enabled) {
+
+        checkSettings();
+
+        assert settingsManager != null;
+        settingsManager.setProperty(ConfigHolder.ALWAYS_ENABLED, enabled);
+
+    }
+
+    /**
+     * @param worldName The world name.
+     * @return True if the world name is added into the config file,
+     *         False if it is already contained.
+     */
+    public static boolean addWorld(@NotNull String worldName) {
+
+        if (worldName.isEmpty()) {
+            throw new IllegalArgumentException("The world name argument cannot be empty");
+        }
+
+        if (containsWorld(worldName)) {
+            return false;
+        }
+
+        assert settingsManager != null;
+        Set<String> worlds = new HashSet<>(getWorlds());
+        worlds.add(worldName);
+
+        settingsManager.setProperty(ConfigHolder.WORLDS, worlds);
+        settingsManager.save();
+
+        return true;
+
+    }
+
+    /**
+     * @param worldName The world name.
+     * @return True if the world name is removed from the config file,
+     *         False if isn't contained.
+     */
+    public static boolean removeWorld(@NotNull String worldName) {
+
+        if (worldName.isEmpty()) {
+            throw new IllegalArgumentException("The world name argument cannot be empty");
+        }
+
+        if (!containsWorld(worldName)) {
+            return false;
+        }
+
+        assert settingsManager != null;
+        Set<String> worlds = new HashSet<>(getWorlds());
+        worlds.remove(worldName);
+
+        settingsManager.setProperty(ConfigHolder.WORLDS, worlds);
+        settingsManager.save();
+
+        return true;
+
+    }
+
+    //
+    // PRIVATE METHODS
+    //
+
+    /**
+     * @throws IllegalStateException If the settings manager instance is null,
+     *                               so if the configuration is not loaded.
+     */
+    private static void checkSettings() {
+
+        if (settingsManager != null) {
+            return;
+        }
+
+        throw new IllegalStateException("the config " + CONFIG_FILE_NAME + " is not loaded");
 
     }
 
--- config/src/main/java/it/gamerover/nfps/config/holder/ConfigHolder.java
+++ config/src/main/java/it/gamerover/nfps/config/holder/ConfigHolder.java
@@ -1,8 +1,41 @@
 package it.gamerover.nfps.config.holder;
 
+import ch.jalu.configme.Comment;
 import ch.jalu.configme.SettingsHolder;
+import ch.jalu.configme.properties.Property;
+import it.gamerover.nfps.config.property.StringSetProperty;
 
+import java.util.*;
+
+import static it.gamerover.nfps.util.YamlUtil.*;
+import static ch.jalu.configme.properties.PropertyInitializer.*;
+
+@SuppressWarnings("squid:S2386") // SonarLint: Make DEF_WORLDS member "protected".
 public class ConfigHolder implements SettingsHolder {
+
+    public static final double      DEF_RADIUS         = 2d;
+    public static final boolean     DEF_ALWAYS_ENABLED = false;
+    public static final Set<String> DEF_WORLDS         = new HashSet<>(Collections.singletonList("YourWorldName"));
+
+    @Comment("The radius of the circle between players where to prevent the sound of footsteps")
+    public static final Property<Double> RADIUS;
+
+    @Comment("True if you want to enable this feature throughout the worlds")
+    public static final Property<Boolean> ALWAYS_ENABLED;
+
+    @Comment({
+            "If the always-enabled property is false, this list will prevent",
+            "footstep sounds throughout the listed worlds"
+    })
+    public static final Property<Set<String>> WORLDS;
+
+    static {
+
+        RADIUS         = newProperty(createProperty("radius"),            DEF_RADIUS);
+        ALWAYS_ENABLED = newProperty(createProperty("always", "enabled"), DEF_ALWAYS_ENABLED);
+        WORLDS         = new StringSetProperty(createProperty("worlds"),  DEF_WORLDS);
+
+    }
 
     private ConfigHolder() {
         // nothing to do
